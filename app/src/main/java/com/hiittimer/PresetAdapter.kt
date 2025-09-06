715536fda49e8f3a80a1aa43098440bbaea446bf
Yaqubazid <yaqub@live.co.uk>
2025-09-06 21:52:21 +0100

    fix windowLightStatusBar, bin icon

--- app/src/main/java/com/hiittimer/PresetAdapter.kt
+++ app/src/main/java/com/hiittimer/PresetAdapter.kt
@@ -1,12 +1,16 @@
 package com.hiittimer
 
 import android.content.Context
+import android.graphics.PorterDuff
+import android.graphics.PorterDuffColorFilter
+import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.ImageButton
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import java.util.Locale
 import java.util.concurrent.TimeUnit
 
@@ -30,15 +34,27 @@ class PresetAdapter(
 
         val preset = getItem(position) as WorkoutPreset
         val titleTextView = view.findViewById<TextView>(R.id.preset_title_textview)
-        val totalTimeTextView = view.findViewById<TextView>(R.id.preset_total_time_textview) // New TextView
+        val totalTimeTextView = view.findViewById<TextView>(R.id.preset_total_time_textview)
         val deleteButton = view.findViewById<ImageButton>(R.id.delete_preset_button)
 
         titleTextView.text = preset.title
 
-        // Calculate and set total time for the preset
         val presetTotalTimeSeconds = (preset.workTimeSeconds + preset.restTimeSeconds) * preset.sets +
                                    preset.getReadyTimeSeconds + preset.coolDownTimeSeconds
         totalTimeTextView.text = formatTime(presetTotalTimeSeconds)
+
+        // Programmatically set the tint by creating a new tinted drawable
+        // Using the user's added ic_baseline_delete_24.xml and SRC_IN mode
+        val originalDrawable: Drawable? = ContextCompat.getDrawable(context, R.drawable.ic_baseline_delete_24) // Changed to custom icon
+        originalDrawable?.let {
+            val tintColor = ContextCompat.getColor(context, R.color.icon_tint_color)
+            val tintedDrawable = it.mutate() // Mutate to avoid affecting other instances of this drawable
+            
+            tintedDrawable.clearColorFilter() // Clear any existing filter
+            tintedDrawable.colorFilter = PorterDuffColorFilter(tintColor, PorterDuff.Mode.SRC_IN) // SRC_IN for vector tinting
+            
+            deleteButton.setImageDrawable(tintedDrawable)
+        }
 
         view.setOnClickListener {
             onPresetLoad(preset)
